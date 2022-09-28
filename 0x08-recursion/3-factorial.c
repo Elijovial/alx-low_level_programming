1595768530973d3f691e510558c1f4012a604f55
Elijovial <omeehelijah345@gmail.com>
2022-09-28 11:29:01 -0700

    Task 3. You mustn't be afraid to dream a little bigger, darling

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -4,16 +4,13 @@
  * factorial - Calculate the factorial of a number
  * @n: the number to caculate the factorial
  *
- * Return: integer value
+ * Return: integer value or -1
  */
 int factorial(int n)
 {
-if (n < 0)
-{
-return (-1);
-}
-if (n <= 1);
-{
-return (n *factorial(n - 1));
-}
+	if (n < 0)
+		return (-1);
+	if (n > 0)
+		return (n * factorial(n - 1));
+	return (1);
 }

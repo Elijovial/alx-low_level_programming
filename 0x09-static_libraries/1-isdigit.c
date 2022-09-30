f33b7daf1d8f201e3a414162bca80e7a82e6bc0a
Elijovial <omeehelijah345@gmail.com>
2022-09-30 16:39:32 -0700

    Task 0. A library is not a luxury but one of the necessities of life

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,14 +1,13 @@
-#include "main.h"
-
 /**
- * _isdigit - Check for digits (0 through 9)
- * @c: int type param
- * Return: 1 if digit, else 0
+ * _isdigit - determine if digit
+ *
+ * @c: character to determine
+ *
+ * Return: gives boolean
  */
 int _isdigit(int c)
 {
-	if (c > 47 && c < 58)
+	if (c >= '0' && c <= '9')
 		return (1);
-
 	return (0);
 }

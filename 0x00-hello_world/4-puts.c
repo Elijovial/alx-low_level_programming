3a7ed2c0f674b143bd32b379cbd62a85c4d46fb3
Elijovial <omeehelijah345@gmail.com>
2023-05-19 10:03:43 -0700

    Task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
-#!/bin/bash
 #include <stdio.h>
-/* the program start here */
+/*
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+
 int main(void)
 {
+	/* The program proper*/
 	char str[] = "Programming is like building a multilingual puzzle.\n";
 
 	puts(str);

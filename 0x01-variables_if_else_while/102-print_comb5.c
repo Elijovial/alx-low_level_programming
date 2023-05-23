e10ffbe2bbebe8349a1e82e8cd5ecbe7c1de1f93
Elijovial <omeehelijah345@gmail.com>
2023-05-22 22:44:38 -0700

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,38 +1,31 @@
 #include <stdio.h>
 
 /**
- *main - print a num pair from 00-99 but no repeats (00 01, 00 02, 00 03,...)
- *Return: Always 0 (Success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int tens;
-	int ones;
-	int t;
-	int o;
+int i, j;
 
-for (tens = '0'; tens <= '9'; tens++)
-/*print first two digit combo*/
-	{
-for (ones = '0'; ones <= '9'; ones++)
+for (i = 0; i < 100; i++)
 {
-for (t = tens; t <= '9'; t++) /*print second of pair*/
+for (j = 0; j < 100; j++)
 {
-for (o = ones + 1; o <= '9'; o++)
+if (i < j)
 {
-putchar(tens);
-putchar(ones);
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
 putchar(' ');
-putchar(t);
-putchar(o);
-if (!((tens == '9' && ones == '8') && (t == '9' && o == '9')))
+putchar((j / 10) + 48);
+putchar((j % 10) + 48);
+if  (i != 98 || j != 99)
 {
 putchar(',');
 putchar(' ');
 }
-}
-o = '0';
 }
 }
 }

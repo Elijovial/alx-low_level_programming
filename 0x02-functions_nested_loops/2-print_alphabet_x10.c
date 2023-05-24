ac3dde2c9937f3c5de8a17b69b97785621e14575
Elijovial <omeehelijah345@gmail.com>
2023-05-24 05:23:18 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - prints lowercase alphabet 10x
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	int i;
+char c;
+for (i = 1; i <= 10; i++)
+{
+for (c = 'a'; c <= 'z'; ++c)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}

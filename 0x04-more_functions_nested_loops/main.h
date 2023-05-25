f967b428a64e35a98276a551a9bed03d3d0e25dc
Elijovial <omeehelijah345@gmail.com>
2023-05-25 09:03:15 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+ * Return: in success 1
+ */
+
+int _putchar(int c);
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: digit to be checked
+ * Return: 1 if true and 0 if false
+ */
+
+int _isupper(int c);

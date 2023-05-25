80f0f94f4e319e1526f6ca18a2541be579921781
Elijovial <omeehelijah345@gmail.com>
2023-05-25 14:10:38 -0700

    Task

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,26 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/**
- * _putchar - puts character to standard output
- * @c: character to put to standard output
- * Return: in success 1
- */
-
 int _putchar(char c);
-
-/**
- * _isupper - checks for uppercase character
- * @c: digit to be checked
- * Return: 1 if true and 0 if false
- */
-
 int _isupper(int c);
-
-/**
- * _isdigit - checks for a digit through (0-9)
- * @c: digit to be checked
- * Return: 1 if c is a digit and 0 if otherwise
- */
-
 int _isdigit(int c);

6cdbafb22bfa5a33f933e18a4504c9c3fd0599f6
Elijovial <omeehelijah345@gmail.com>
2023-05-25 05:32:16 -0700

    Task

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,7 +3,12 @@
 
 #include <stdio.h>
 
-
+/**
+ * positive_or_negative - tests function that prints if
+ * integer is positive or negative.
+ * @i: integer argument
+ * Return: 0 success
+ */
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */

2899acbab4717e28cae1be440c243181f04e9ff5
Elijovial <omeehelijah345@gmail.com>
2023-05-26 05:44:00 -0700

    Task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,11 +4,12 @@
  * mul - multiplies two integers
  * @a: integer to be multiplied
  * @b: integer to be multiplied
- * Return: 0 success;
+ * Return: success
  */
 
 int mul(int a, int b)
 {
-	_putchar(a * b);
-	return (a * b);
+	int c;
+
+	c = a * b;
 }

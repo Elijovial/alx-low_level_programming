e92b898ecd1469def720d03146ba35d7aec0acce
Elijovial <omeehelijah345@gmail.com>
2023-05-30 09:40:45 -0700

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,12 +8,16 @@
 
 void print_rev(char *s)
 {
-int i = 62;
+int i = 0;
+int j;
 
 	while (s[i] != '\0')
 	{
-	_putchar(s[i]);
-	--i;
+	i++;
+	}
+	for (j = i - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
 	}
 _putchar('\n');
 }

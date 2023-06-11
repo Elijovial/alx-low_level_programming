7eec472a0332a1ededf815a7a347b4cd5024ab40
Elijovial <omeehelijah345@gmail.com>
2023-06-11 09:05:38 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - The program start here
+ * @argc: integer argument
+ * @argv: string argument
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	printf("Argument count = %d\n", argc);
+	return (0);
+}

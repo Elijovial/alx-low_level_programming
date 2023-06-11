7dde905677ade3483e3aad4aa317b5645b579c81
Elijovial <omeehelijah345@gmail.com>
2023-06-11 11:26:27 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - The program start here
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("Argument count = %d\n", argc - 1);
+	return (0);
+}

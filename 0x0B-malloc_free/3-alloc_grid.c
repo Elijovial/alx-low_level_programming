c0380b47784af6d6c6e6e0ca5023636f3da2e6ef
Elijovial <omeehelijah345@gmail.com>
2023-06-17 10:21:35 -0700

    Task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -29,9 +29,9 @@ int **alloc_grid(int width, int height)
 			for (j = 0; j < i; j++)
 			{
 				free(arr[j]);
+			}
 				free(arr);
 				return (NULL);
-			}
 		}
 		for (j = 0; j < width; j++)
 			arr[i][j] = 0;

906e867198f64ac50ab560e2a6c3519a71aac3aa
Elijovial <omeehelijah345@gmail.com>
2023-06-20 11:17:40 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - Defines a new type
+ * @name: Dog name
+ * @age: Dog's age
+ * @owner: The owner of the dog
+ * Return: Always 0
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

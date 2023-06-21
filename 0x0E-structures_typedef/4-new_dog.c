714fcc2f969aab75f56af19e9b6ee6bfb2542c47
Elijovial <omeehelijah345@gmail.com>
2023-06-21 05:55:42 -0700

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -27,12 +27,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->name = strdup(name);
 	if (new_dog->name == NULL)
 	{
+		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
 	new_dog->owner = strdup(owner);
 	if (new_dog->owner == NULL)
 	{
+		free(new_dog->owner);
 		free(new_dog->name);
 		free(new_dog);
 		return (NULL);

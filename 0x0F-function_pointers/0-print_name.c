a311c265708aa3390430b3a4148fc47eaf34f4ae
Elijovial <omeehelijah345@gmail.com>
2023-06-21 15:39:43 -0700

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -9,14 +9,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
-}
-/**
- * print - funtion that prints
- * @name: pointer to character
- * Return: Nothing
- */
-void print(char *name)
-{
-	printf("%s\n", name);
+	if (name != NULL || f != NULL)
+		f(name);
 }

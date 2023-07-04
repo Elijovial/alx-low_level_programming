388759d959752275e1b0a69501254c57c0246018
Elijovial <omeehelijah345@gmail.com>
2023-07-03 21:59:05 -0700

    Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list.
+ * @head: pointer head
+ * Return:
+ */
+
+void free_list(list_t *head)
+{
+	list_t *p;
+
+	while (head != NULL)
+	{
+		p = head;
+		head = head->next;
+		free(p);
+	}
+}

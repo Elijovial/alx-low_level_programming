8d80594bbff2d2adf01eefd029630cda5d72b546
Elijovial <omeehelijah345@gmail.com>
2023-08-18 22:04:39 -0700

    Task 3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -28,11 +28,11 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	}
 
 	last_node = *head;
-    while (last_node->next != NULL)
-        last_node = last_node->next;
+	while (last_node->next != NULL)
+		last_node = last_node->next;
 
-    last_node->next = new_node;
-    new_node->prev = last_node;
+	last_node->next = new_node;
+	new_node->prev = last_node;
 
-    return (new_node);
+	return (new_node);
 }

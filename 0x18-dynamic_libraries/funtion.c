9d37c90a2cd355405dc3db76152d8ac40ef7455c
Elijovial <omeehelijah345@gmail.com>
2023-08-21 19:06:28 -0700

    Task 2

---
+++ 0x18-dynamic_libraries/funtion.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+ * add - function that add two numbers
+ * @a: first integer
+ * @b: second integer
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - function that subtract two numbers
+ * @a: first integer
+ * @b: second integer
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - function that multiply two numbers
+ * @a: first integer
+ * @b: second integer
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - function that divide two numbers
+ * @a: first integer
+ * @b: second integer
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - function that gets the modulos of two numbers
+ * @a: first integer
+ * @b: second integer
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}

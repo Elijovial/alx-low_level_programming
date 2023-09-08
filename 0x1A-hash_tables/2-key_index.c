d3223fdb2f88c934932e5de0e6574bb409713fae
Elijovial <omeehelijah345@gmail.com>
2023-09-08 16:20:46 -0700

    Task 2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -8,13 +8,13 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int hash;
-    unsigned long int index;
+	unsigned long int hash;
+	unsigned long int index;
 
-    /* Compute the hash code of the key using hash_djb2 */
-    hash = hash_djb2(key);
-    /* Reduce the hash code to an index using modulo */
-    index = hash % size;
-    /* Return the index */
-    return (index);
+	/* Compute the hash code of the key using hash_djb2 */
+	hash = hash_djb2(key);
+	/* Reduce the hash code to an index using modulo */
+	index = hash % size;
+	/* Return the index */
+	return (index);
 }

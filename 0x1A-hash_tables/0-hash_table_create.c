ab15e57a047a0ef8c2a73a18da2221bd79d3ffee
Elijovial <omeehelijah345@gmail.com>
2023-09-08 15:13:58 -0700

    Task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,30 +8,30 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    /* Declare variables */
-    hash_table_t *ht;
-    unsigned long int i;
+	/* Declare variables */
+	hash_table_t *ht;
+	unsigned long int i;
 
-    /* Allocate memory for the hash table structure */
-    ht = malloc(sizeof(hash_table_t));
-    if (ht == NULL)
-        return (NULL);
+	/* Allocate memory for the hash table structure */
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
 
-    /* Set the size attribute */
-    ht->size = size;
+	/* Set the size attribute */
+	ht->size = size;
 
-    /* Allocate memory for the array of pointers */
-    ht->array = malloc(sizeof(hash_node_t *) * size);
-    if (ht->array == NULL)
-    {
-        free(ht);
-        return (NULL);
-    }
+	/* Allocate memory for the array of pointers */
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
 
-    /* Initialize each pointer to NULL */
-    for (i = 0; i < size; i++)
-        ht->array[i] = NULL;
+	/* Initialize each pointer to NULL */
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
 
-    /* Return the pointer to the hash table */
-    return (ht);
+	/* Return the pointer to the hash table */
+	return (ht);
 }
